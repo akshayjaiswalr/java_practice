5e43ef57a3b8b34e89fa01ab78779c23fdf1426f
akshayjaiswalr <akshayjaiswalr@gmail.com>
2023-06-17 15:56:40 +0530

    adds sample code for encapsulation

---
+++ practice_pro/src/opps_practice/encapsulation/Vehicle.java
@@ -0,0 +1,32 @@
+package opps_practice.encapsulation;
+
+public class Vehicle {
+    private String company;
+    private String type;
+    private String seats;
+
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
+}
