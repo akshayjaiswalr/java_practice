ca5003e54c7d34c9d2d0b002ceb488db74616b38
akshayjaiswalr <akshayjaiswalr@gmail.com>
2023-06-17 16:13:28 +0530

    adds sample code for abstraction and method for area of square

--- practice_pro/src/Main.java
+++ practice_pro/src/Main.java
@@ -1,3 +1,5 @@
+import opps_practice.abstraction.Circle;
+import opps_practice.abstraction.Square;
 import opps_practice.encapsulation.Vehicle;
 
 public class Main {
@@ -9,8 +11,12 @@ public class Main {
             System.out.println(car.getCompany());
             System.out.println(car.getType());
             System.out.println(car.getSeats());
-
-
+        Circle circle=new Circle();
+        circle.setRadius(2.4);
+        System.out.println(circle.area());
+        Square square=new Square();
+        square.setSide(3.5);
+        System.out.println(square.area());
     }
 
 }
